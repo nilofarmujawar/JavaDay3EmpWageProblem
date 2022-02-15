731da80c1311435eddd14237ff32ab0e2474461f
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-16 02:56:05 +0530

    calculate wages till a condition of total working hours or days is reached for a month

--- src/empWage/EmpWage.java
+++ src/empWage/EmpWage.java
@@ -12,15 +12,15 @@ public class EmpWage {
         
         Random rand = new Random();
 
-        int empHrs;
+        int empHrs=0;
+        
+        int salary;
         
         int present = 1;
         
         int absent = 2;
         
         int part_time_present = 3;
-        
-        int check=rand.nextInt(3)+1;
         
         int wage_per_hr = 20;
         
@@ -30,31 +30,51 @@ public class EmpWage {
         
         int max_working_days = 20;
         
-        int max_working_hrs = 20;
+        int max_working_hrs = 100;
+        
+        int working_hrs=0;
+        
+        int working_days=0;
 
-        if (check == present) {
+        while (working_hrs < max_working_hrs && working_days < max_working_days) {
         	
-            System.out.println("Employee is present.");
+            int check = rand.nextInt(3) + 1;
+
+            if (check == present) {
+            	
+                System.out.println("Employee is present.");
+                
+            } else if (check == part_time_present) {
+            	
+                System.out.println("Employee is present for part time.");
+                
+            } else {
+            	
+                System.out.println("Employee is absent.");
+                
+            }
+
+            switch (check) {
+
+                case 1 -> empHrs = 8;
+                case 2 -> empHrs = 0;
+                case 3 -> empHrs = 4;
+                
+            }
+            
+            int this_day = empHrs*wage_per_hr;
+            
+            System.out.println(this_day);
+            
+            working_hrs = working_hrs + empHrs;
+            
+            working_days = working_days + 1;
             
         }
         
-        else if (check == part_time_present) {
-        	
-            System.out.println("Employee is present for part time.");
-        }
-
-        else {
-        	
-            System.out.println("Employee is absent.");
-            
-        }
-
-        switch (check) {
-            case 1 -> empHrs = 8;
-            case 2 -> empHrs = 0;
-            case 3 -> empHrs = 4;
-        }
-
-
+        salary = working_hrs * wage_per_hr;
+        
+        System.out.println("salary of month is : " + salary);
+        
     }
 }
